9a1bd21b01a1dc26773bfeb45be0384dc09f7c8c
Eske Wolff <Eske Wolff@10.20.172.168>
2016-04-13 12:38:13 +0200

    First commit wedensday. CompanyInfo with get

--- web/app/viewCompanyInfo/viewCompanyInfo.js
+++ web/app/viewCompanyInfo/viewCompanyInfo.js
@@ -2,16 +2,41 @@
 
 angular.module('myApp.viewCompanyInfo', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/CompanyInfo', {
-    templateUrl: 'app/viewCompanyInfo/viewCompanyInfo.html',
-    controller: 'ViewCompanyInfoCtrl',
-    controllerAs : 'ctrl'
-  });
-}])
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/CompanyInfo', {
+                    templateUrl: 'app/viewCompanyInfo/viewCompanyInfo.html',
+                    controller: 'ViewCompanyInfoCtrl',
+                    controllerAs: 'ctrl'
+                });
+            }])
 
-.controller('ViewCompanyInfoCtrl', [function() {
-  var self = this;
-   self.message = "HELLO FROM THE OTheR SIIIIIIIDE!!";
-   self.joni = "Joni ER Stadig bøsse.";
-}]);
+        .controller('ViewCompanyInfoCtrl', ["$http", function ($http) {
+                var self = this;
+                self.message = "HELLO FROM THE OTheR SIIIIIIIDE!!";
+                self.joni = "Joni ER Stadig bøsse.";
+
+                self.company = {};
+
+                self.getCompany = function () {
+                    return $http({
+                        method: 'GET',
+                        url: 'https://cvrapi.dk/api?vat=' + self.cvr + '&country=DK',
+                        skipAuthorization: true,
+                        contentType: "application/json"
+                    }).success(function (data) {
+                        self.company = data;
+                        console.log(data);
+                    }).
+                            error(function (data) {
+                            });
+                };
+            }]);
+
+
+//    $http.get('api/demouser')
+//        .success(function(data) {
+//            self.data = data;
+//    console.log(data);
+//        })
+//        .error(function(data) {
+//        });
